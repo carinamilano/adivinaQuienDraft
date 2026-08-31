45b283f56ad98f1d99f0564431654bc3191646ec
Carina Milano <carinamilano@gmail.com>
2026-08-30 22:17:49 -0300

    main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
+import motor.GestorPartida;
+
+/**
+ * Punto de entrada del programa. Su unica responsabilidad es crear el
+ * GestorPartida y arrancarlo.
+ */
+
 public class Main {
+    public static void main(String[] args)
+    {
+        GestorPartida partida = new GestorPartida();
+        partida.iniciar();
+    }
 }
